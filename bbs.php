dfed8a5c1f78c35b61902c1f54ac6fe52e8cfe38
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2017-02-11 00:49:43 +0900

    Handle post request

--- bbs.php
+++ bbs.php
@@ -6,6 +6,38 @@ if (!$link) {
 }
 
 mysql_select_db('oneline_bbs', $link);
+
+$errors = array();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Validete name
+  $name = null;
+  if (!isset($_POST['name']) || !strlen($_POST['name'])) {
+    $errors['name'] = '名前を入力してください';
+  } else if (strlen($_POST['name']) > 40) {
+    $errors['name'] = '名前は40文字以内で入力してください';
+  } else {
+    $name = $_POST['name'];
+  }
+
+  // Validate comment
+  $comment = null;
+  if (!isset($_POST['comment']) || !strlen($_POST['comment'])) {
+    $errors['comment'] = 'ひとことを入力してください';
+  } else if (strlen($_POST['comment']) > 200) {
+    $errors['comment'] = 'ひとことは200文字以内で入力してください';
+  } else {
+    $comment = $_POST['comment'];
+  }
+
+  // Save
+  if (count($errors) === 0) {
+    $sql = "INSERT INTO `post` (`name`, `comment`, `created_at`) VALUES ('"
+      . mysql_real_escape_string($name) . "', '"
+      . mysql_real_escape_string($comment) . "', '"
+      . date('Y-m-d H:i:s') . "')";
+    mysql_query($sql, $link);
+  }
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
